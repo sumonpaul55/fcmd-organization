04d3ec7358ced5bfc8e803334fa9fcb2cdeb0b79
sumonpaul55 <sumpaul100@gmail.com>
2024-04-01 20:39:06 +0600

    readme and mostpopular post

--- src/pages/blogs/mostPopular/MostPopular.jsx
+++ src/pages/blogs/mostPopular/MostPopular.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import SectionTitle from './../../../components/sectionTitle/SectionTitle';
 import { useQuery } from '@tanstack/react-query';
